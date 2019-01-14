97956b4385f39a86192a648479e1a6844e5066a9
Carolin Bruederle <rumpf@team23.de>
2019-01-14 10:17:29 +0100

    initial commit

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    article = [
+        {
+            'id': 1,
+            'title': 'Article 1',
+            'body': 'First Article and much fun to read ...',
+            'author': 'Mister Magic',
+            'create date': '04-12-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article 2',
+            'body': 'Second Article and much fun to read ...',
+            'author': 'Mister Magic',
+            'create date': '04-12-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article 3',
+            'body': 'Third Article and much fun to read ...',
+            'author': 'Mister Magic',
+            'create date': '04-12-2018'
+        }
+    ]
+
+    return article
